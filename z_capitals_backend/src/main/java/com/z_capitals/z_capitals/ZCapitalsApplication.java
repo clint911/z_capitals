fcdf42efcf8a545c8373345665c2b07f33603ca9
clint911 <clingyking007@gmail.com>
2025-12-03 20:52:25 +0300

    restructuring

---
+++ z_capitals_backend/src/main/java/com/z_capitals/z_capitals/ZCapitalsApplication.java
@@ -0,0 +1,13 @@
+package com.z_capitals.z_capitals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZCapitalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZCapitalsApplication.class, args);
+	}
+
+}
---
+++ z_capitals_backend/src/test/java/com/z_capitals/z_capitals/ZCapitalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.z_capitals.z_capitals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZCapitalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
